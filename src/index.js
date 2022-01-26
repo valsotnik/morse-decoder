c8a345141641488027522ba02b38c172555b9b96
valsotnik <cmvaltime@gmail.com>
2022-01-26 12:32:02 +0300

    task complete

--- src/index.js
+++ src/index.js
@@ -35,10 +35,31 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+		'**********':  ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let array = [];
+		for (let i = 0; i < expr.length; i += 10) {
+			array.push(expr.slice(i, i + 10))
+		} 
+
+		for (let i = 0; i < array.length; i++) {
+			if (array[i][0] === '0') {
+				array[i] = String(+array[i])
+			}
+			array[i] = array[i].replace(/10/g, '.').replace(/11/g, '-'); 
+		}
+
+		for (let i = 0; i < array.length; i++) {
+			for (let key in MORSE_TABLE) {
+				if (array[i] === key) {
+					array[i] = MORSE_TABLE[key];
+				}
+				
+			}
+		}
+	return array.join('')	
 }
 
 module.exports = {
